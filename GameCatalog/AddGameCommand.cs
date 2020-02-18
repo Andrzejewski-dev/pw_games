adb76c0ff3092328fa22d068551b09cdc781397b
Kamil <kamil.andrzejewski@as4you.pl>
2020-02-18 21:24:16 +0100

    init

---
+++ GameCatalog/AddGameCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GameCatalog
+{
+    public class AddGameCommand:ICommand
+    {
+        private GameListViewModel _gameListVM;
+        public AddGameCommand(GameListViewModel gameListVM)
+        {
+            _gameListVM = gameListVM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _gameListVM.AddNewGame();
+        }
+    }
+}
---
+++ GamesInterface/IDirectory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameInterfaces
+{
+    public interface ICatalog
+    {
+        List<IGame> GetAllGames();
+    }
+}
